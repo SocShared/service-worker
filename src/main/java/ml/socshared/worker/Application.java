b0526dd71d7e40b6b356a6842773a56b1c2a3128
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-06 04:36:41 +0300

    Пофиксил Scheduled

--- src/main/java/ml/socshared/worker/Application.java
+++ src/main/java/ml/socshared/worker/Application.java
@@ -6,8 +6,10 @@ package ml.socshared.worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
