6af98c3b52302fbecf659c27b6084cc7e0eac50c
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-07 18:02:46 +0300

    Добавил ToString в GroupPostStatus@

--- src/main/java/ml/socshared/worker/domain/storage/GroupPostStatus.java
+++ src/main/java/ml/socshared/worker/domain/storage/GroupPostStatus.java
@@ -3,11 +3,13 @@ package ml.socshared.worker.domain.storage;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@ToString
 public class GroupPostStatus {
 
     private UUID groupId;
