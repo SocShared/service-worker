c5df8c6cb215212950c9420d179fe75300fd9f13
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-09 23:16:39 +0300

    Фикс обработки исключений

--- src/main/java/ml/socshared/worker/service/impl/WorkerServiceScheduledImpl.java
+++ src/main/java/ml/socshared/worker/service/impl/WorkerServiceScheduledImpl.java
@@ -122,7 +122,7 @@ public class WorkerServiceScheduledImpl implements WorkerServiceScheduled {
                     }
                 }
             } catch (AbstractRestHandleableException | RetryableException exc) {
-                String mes = "Connection refused: " + exc.getMessage();
+                String mes = "conn. ref.: " + exc.getMessage();
                 if (exc instanceof AbstractRestHandleableException) {
                     Map<String, Object> mapError = objectMapper.readValue(exc.getMessage(), HashMap.class);
                     mes = (String) mapError.get("message");
