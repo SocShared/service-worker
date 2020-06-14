8a15328f3c4765a6d4532a3029fb2efbafed1328
nrx <ageevav97@gmail.com>
2020-06-14 18:32:10 +0300

    Добавлен сбор статитсики в ВК

--- src/main/java/ml/socshared/worker/service/VkService.java
+++ src/main/java/ml/socshared/worker/service/VkService.java
@@ -1,6 +1,7 @@
 package ml.socshared.worker.service;
 
 import ml.socshared.worker.domain.vk.request.VkPostRequest;
+import ml.socshared.worker.domain.vk.response.VkGroupResponse;
 import ml.socshared.worker.domain.vk.response.VkPostResponse;
 
 import java.util.UUID;
@@ -8,5 +9,8 @@ import java.util.UUID;
 public interface VkService {
 
     VkPostResponse savePost(UUID systemUserId, String groupId, VkPostRequest request);
+    VkPostResponse getPostOfGroupById(UUID systemUserId, String socGroupId, String socPostId);
+    VkGroupResponse getGroupById(UUID systemUserId, String socGroupId);
+    Integer getGroupOnline(UUID systemUserId, String socGroupId);
 
 }
